844c5b094996d6022c961e4412175820876e0a6a
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-11 21:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function calculateGrade(percentage) {
   //your code here
+	 if (typeof percentage !== 'number' || isNaN(percentage)) {
+        return 'F';
+    }
+
+    // Round the percentage to the nearest integer
+    const roundedPercentage = Math.round(percentage);
+
+    // Determine the letter grade based on the percentage score
+    if (roundedPercentage >= 90) {
+        return 'A';
+    } else if (roundedPercentage >= 80) {
+        return 'B';
+    } else if (roundedPercentage >= 70) {
+        return 'C';
+    } else if (roundedPercentage >= 60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
